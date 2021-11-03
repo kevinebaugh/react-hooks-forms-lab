361f116137827b4c94aeacddc7fc849876ab52d9
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-11-02 21:59:53 -0400

    commit tree 3839d3041dc1d81e2a25d2d7ae632ff50ea5f493

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,24 +2,45 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+  const [statefulItems, setStatefulItems] = useState(items)
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function onSearchChange(event) {
+    setSearch(event.target.value)
+  }
+
+  function onItemFormSubmit(event) {
+    event.preventDefault()
+    const itemName = event.target.name.value
+    const itemCategory = event.target.category.value
+    const newItem = {
+      id: uuid(), // the `uuid` library can be used to generate a unique id
+      name: itemName,
+      category: itemCategory,
+    }
+    setStatefulItems([...statefulItems, newItem])
+  }
+
+  const itemsToDisplay = statefulItems.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+  }).filter((item) => {
+    if (search.length == 0 || item.name.toLowerCase().includes(search.toLowerCase())) return true
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} search={search} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
